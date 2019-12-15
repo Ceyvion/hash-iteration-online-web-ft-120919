62a4e24eabbe136b66d82bb77f10007da4fc942d
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-15 21:52:38 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,9 +12,9 @@ def select_winner(passengers)
       if suite == :suite_a && name.start_with?("A")
         winner = name
       end
+      winner
     end
-     
-    winner
+
   
   
 def happy_birthday(birthday_kids)
